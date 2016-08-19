6f6743b4616765c5d7e4641093f1b2b98c9b939c
mahaigang <mahaigang@chinasofti.com>
2016-08-19 18:02:23 +0800

    单个数据库

--- src/main/java/com/mahg/jf/jfes/jfesTest/App.java
+++ src/main/java/com/mahg/jf/jfes/jfesTest/App.java
@@ -3,11 +3,8 @@ package com.mahg.jf.jfes.jfesTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
-
-import com.mahg.jf.jfes.dao.DynamicDataSourceRegister;
 import com.mahg.jf.jfes.service.UserService;
 
 /**
@@ -15,7 +12,6 @@ import com.mahg.jf.jfes.service.UserService;
  *
  */
 @SpringBootApplication
-@Import(DynamicDataSourceRegister.class)
 @ImportResource(value = {"classpath:spring-idx.xml"})
 
 public class App 
@@ -24,13 +20,8 @@ public class App
     
     {
     	ApplicationContext application = SpringApplication.run(App.class, args);
-    	AppDataSourceConstant.APP_DATA_SOURCE="devDataSource";
     	UserService userService=(UserService) application.getBean("userService");
     	userService.findUserByjfid("mahg");
         System.out.println( "Hello World!" );
-        AppDataSourceConstant.APP_DATA_SOURCE="testDataSource";
-        UserService userService1=(UserService) application.getBean("userService");
-    	userService1.findUserByjfid("mahg");
-        System.out.println( "Hello World2!" );
     }
 }
--- src/main/java/com/mahg/jf/jfes/service/impl/UserServiceImpl.java
+++ src/main/java/com/mahg/jf/jfes/service/impl/UserServiceImpl.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.mahg.jf.jfes.dao.DevInfoMapper;
-import com.mahg.jf.jfes.dao.DynamicDataSourceRegister;
 import com.mahg.jf.jfes.service.UserService;
 
 
